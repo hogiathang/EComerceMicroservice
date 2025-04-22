a4c1cdee9b50b2ebc792e043e033bb1befc786d8
RavenTheshadow <hogiathang2@gmail.com>
2025-04-22 09:35:55 +0700

    feat: backend-gateway-routing

---
+++ Backend/gateway/src/main/java/com/ecomerce/gateway/GatewayApplication.java
@@ -0,0 +1,39 @@
+package com.ecomerce.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.gateway.route.RouteLocator;
+
+import java.time.LocalDateTime;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p-> p
+						.path("/account/**")
+						.filters(f -> f
+								.rewritePath("/account/(?<segment>.*)", "/api/v1/account/${segment}")
+								.addRequestHeader("X-Response-Header", LocalDateTime.now().toString())
+						)
+						.uri("lb://user-service"))
+
+				.route(p-> p
+						.path("/register", "/login")
+						.filters(f-> f
+								.rewritePath("/register", "/api/v1/account/register")
+								.rewritePath("/login", "/api/v1/account/login"))
+						.uri("lb://user-service"))
+				.build();
+	}
+}
